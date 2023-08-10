c2831adc4e5ed35b48ca4a28f4da23accab7d94f
Tatiana Valanitskaya <tatiana010fi@gmail.com>
2023-08-10 12:37:38 +0300

    Change handleUpload, add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {UploadComponent} from "./UploadComponent";
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <UploadComponent/>
     </div>
   );
--- src/UploadComponent.tsx
+++ src/UploadComponent.tsx
@@ -1,53 +1,72 @@
-import React, { useState } from 'react';
-import { Upload, Button } from 'antd';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Button, Upload, message } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+
+const MAX_FILE_COUNT = 100;
 
 export const UploadComponent: React.FC = () => {
     const [fileList, setFileList] = useState([]);
     const [selectedFileCount, setSelectedFileCount] = useState(0);
 
-    const handleFileChange = (info: any) => {
-        let fileList:any = [...info.fileList].slice(-100); // Ограничить количество файлов до 100
-        fileList = fileList.map((file: any) => {
-            if (file.response) {
-                file.url = file.response.url; // Сохранить URL загруженного файла
-            }
-            return file;
-        });
+    const handleUpload = () => {
+        const uploadFiles = async (fileList: any[]) => {
+            try {
+                const uploadUrl = 'https://cloud-api.yandex.net/v1/disk/resources/upload'; // Yandex.Disk upload API endpoint
+                const uploadPromises = fileList.map(async (file) => {
+                    const formData = new FormData();
+                    formData.append('file', file);
 
-        setFileList(fileList);
-        setSelectedFileCount(fileList.length);
+                    const response = await axios.post(uploadUrl, formData, {
+                        headers: {
+                            'Authorization': 'Bearer YOUR_YANDEX_DISK_API_TOKEN',
+                            'Content-Type': 'multipart/form-data',
+                        },
+                        params: {
+                            path: `/${file.name}`, // Specify the destination path on Yandex.Disk
+                        },
+                    });
+
+                    return response.data;
+                });
+
+                const uploadResults = await Promise.all(uploadPromises);
+                console.log(uploadResults)
+                message.success('Files uploaded successfully');
+            } catch (error) {
+                console.error('Error occurred during file upload:', error);
+            }
+        };
+
+        uploadFiles(fileList);
+
     };
 
-    const handleUpload = () => {
-        // Загружаем файлы на Яндекс.Диск
-        fileList.forEach((file: any) => {
-            const formData = new FormData();
-            formData.append('file', file.originFileObj);
+    const handleFileListChange = (newFileList:any) => {
+        if (newFileList.length > MAX_FILE_COUNT) {
+            message.error(`You can upload up to ${MAX_FILE_COUNT} files`);
+            return;
+        }
 
-            // здесь нужно установить url загрузки файлов на яндекс диск
-            fetch('YANDEX_DISK_UPLOAD_URL', {
-                method: 'POST',
-                body: formData
-            }).then((response) => {
-                // Обработка ответа от сервера
-                console.log(response);
-            });
-        });
+        setFileList(newFileList);
+        setSelectedFileCount(fileList.length);
     };
 
     return (
         <div>
             <Upload
-                onChange={handleFileChange}
                 fileList={fileList}
-                multiple={true}
-                beforeUpload={() => false} // Отключить автоматическую загрузку файлов
+                onChange={(info) => handleFileListChange(info.fileList)}
+                maxCount={MAX_FILE_COUNT}
             >
                 <Button icon={<UploadOutlined />}>Выбрать файлы</Button>
             </Upload>
-
-            <Button type="primary" onClick={handleUpload} disabled={selectedFileCount === 0}>
+            <Button
+                type="primary"
+                disabled={selectedFileCount === 0 }
+                onClick={handleUpload}
+                style={{ marginTop: 16 }}
+            >
                 Загрузить ({selectedFileCount} файлов выбрано)
             </Button>
         </div>
